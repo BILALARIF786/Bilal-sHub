bf5333698fa0881be52640a46998d7d55c1ff9ba
MUHAMMAD BILAL <mbilalarifaziz@gmail.com>
2024-01-10 09:51:53 +0500

    My First Project

---
+++ main.js
@@ -0,0 +1,119 @@
+document.getElementById("btn1").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn2").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn3").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn4").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn5").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn6").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn7").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn8").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn9").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn10").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn11").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn12").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn13").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn14").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn15").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn16").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn17").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn18").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn19").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn20").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn21").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn22").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn23").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn24").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn25").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn26").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn27").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn28").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn29").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
+
+  document.getElementById("btn30").addEventListener('click',function(){
+    alert("Thanks For Purchasing");
+  });
